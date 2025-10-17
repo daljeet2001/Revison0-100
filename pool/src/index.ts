9c28e09d0dc2cfaa387b8d2b3d81ddefcaba6a6b
DALJEET SINGH <daljeetsingh@DALJEETs-MacBook-Air.local>
2025-10-17 11:36:35 +0530

    connection pooling debugged

--- pool/src/index.ts
+++ pool/src/index.ts
@@ -1,26 +1,27 @@
-import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 export interface Env {
   DATABASE_URL: string;
 }
 
 export default {
-  async fetch(request:Request, env:Env, ctx:ExecutionContext) {
-    const prisma = new PrismaClient({
-      datasourceUrl: env.DATABASE_URL,
-    }).$extends(withAccelerate());
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
 
-	console.log(`prisma ${prisma}`)
-  const friend =await prisma.friend.create({
-    data:{
-      name:"daljeet",
-      age:23
+    if(request.method !== "POST"){
+      return new Response ('Only POST Allowed', {status:405});
     }
-  })
+
+    const prisma = new PrismaClient({ datasourceUrl: env.DATABASE_URL, }).$extends(withAccelerate())
+    const friend = await prisma.friend.create({
+      data: {
+        name: "arsh",
+        age: 22
+      }
+    })
 
     // const users = await prisma
     // const result = JSON.stringify(users);
-    return  Response.json(friend);
+    return Response.json(friend);
   },
 } satisfies ExportedHandler<Env>;
